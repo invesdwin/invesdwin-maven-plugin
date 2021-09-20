86f9918c962c345bbd11705bff7cb69b7a686472
Edwin Stang <gsubes@gmail.com>
2021-09-20 13:44:56 +0200

    Update installEclipse.sh

--- installEclipse.sh
+++ installEclipse.sh
@@ -10,7 +10,7 @@ sudo sed -i s/-Xmx[0-9]*m/-Xmx5g/ /opt/eclipse/eclipse.ini
 # create a launcher
 echo "cd /opt/eclipse && ./eclipse \$*" | sudo tee /usr/local/bin/eclipse
 sudo chmod +x /usr/local/bin/eclipse
-# install javahl
+# install javahl (optional)
 sudo apt-get -y install libsvn-java
 echo "-Djava.library.path=/usr/lib/jni:/usr/lib/x86_64-linux-gnu/jni/" | sudo tee -a /opt/eclipse/eclipse.ini
 echo "-Dfile.encoding=UTF-8" | sudo tee -a /opt/eclipse/eclipse.ini
